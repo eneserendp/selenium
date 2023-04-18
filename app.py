b915f6dfb968e4bb39c875ce889d5c068d211275
Enes Eren Demirpolat <enes_erendemirpolat@hotmail.com>
2023-04-18 16:33:39 +0300

    ilk  commit

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+
+
 
 
 
@@ -12,6 +17,7 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
 driver.maximize_window()
 time.sleep(3)
+
 login=driver.find_element("xpath",'//*[@id="username_"]')
 login.click()
 time.sleep(1)
@@ -20,12 +26,54 @@ login.send_keys("gusta@megasav.com")
 password=driver.find_element("xpath",'//*[@id="password_"]')
 password.click()
 time.sleep(1)
-password.send_keys("Mert16")
+password.send_keys("Mert1606")
 
 log_in=driver.find_element("xpath",'//*[@id="loginForm"]/div[4]/div[1]/button')
 
 log_in.click()
 time.sleep(1)
 
+
+continue_link = driver.find_element(By.CLASS_NAME, 'navigation-categories-item-title')
+print(f"Kategori İsmi:{continue_link.text}")
+
 time.sleep(2)
-driver.close()
+achains =ActionChains(driver)
+
+time.sleep(2)
+achains.move_to_element(continue_link).perform()
+time.sleep(2)
+morebutton=driver.find_element(By.CLASS_NAME, 'navigation-categories-dropdown-head-title').click()
+time.sleep(5)
+
+content =driver.find_element("xpath",'/html/body/main/section[2]/div/div/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/a/h4').click()
+
+time.sleep(3)
+
+
+pruduct=driver.find_element(By.CLASS_NAME, 'product-title')
+print(f"Ürün İsmi:{pruduct.text}")
+
+time.sleep(2)
+
+smallTitle=driver.find_element(By.CLASS_NAME, 'product-title-small')
+
+print(f"Kısa Açıklama:{smallTitle.text}")
+
+time.sleep(2)
+
+stokMiktar=driver.find_element("xpath",'/html/body/main/section[2]/div/div/div/div[2]/div[2]/div[2]')
+
+print(stokMiktar.text)
+
+time.sleep(2)
+ImageUrl=driver.find_element(By.CLASS_NAME, 'area-product-image').find_element(By.CSS_SELECTOR,'a').get_attribute('href')
+
+print(f"Resim Url:{ImageUrl}")
+time.sleep(2)
+
+
+
+
+
+
