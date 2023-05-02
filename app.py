5497123b96a95ac198118af5373986eaa037152a
Enes Eren Demirpolat <enes_erendemirpolat@hotmail.com>
2023-05-02 16:34:49 +0300

    yenilik

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.action_chains import ActionChains
+import xml.etree.ElementTree as ET
 
 url = 'https://www.edenge.com.tr/'
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -23,7 +24,6 @@ log_in=driver.find_element("xpath",'//*[@id="loginForm"]/div[4]/div[1]/button')
 log_in.click()
 time.sleep(1)
 continue_link = driver.find_element(By.CLASS_NAME, 'navigation-categories-item-title')
-print(f"Kategori İsmi:{continue_link.text}")
 time.sleep(1)
 achains =ActionChains(driver)
 time.sleep(1)
@@ -36,17 +36,21 @@ content =driver.find_elements(By.CLASS_NAME,"js_obasket_parents")
 # disablePNo = driver.find_elements(By.CLASS_NAME,'PagedList-skipToNext')
 content2=driver.find_element(By.CLASS_NAME,"PagedList-skipToNext").find_element(By.CLASS_NAME,'js_pagelink')
 
+# newContent=driver.find_element(By.CLASS_NAME,"js_obasket_parents").find_element(By.CLASS_NAME,'title-cell').find_element(By.CSS_SELECTOR,'a').get_attribute("href")
+# new2 = newContent.split("/")
+# new3 = new2[5].split("?")
+GenelList=[]
 
 def get_data():
     pruduct=driver.find_element(By.CLASS_NAME, 'product-title')
-    print(f"Ürün İsmi:{pruduct.text}")
+   
     smallTitle=driver.find_element(By.CLASS_NAME, 'product-title-small')
-    print(f"Description:{smallTitle.text}")
+    
     stokMiktar=driver.find_element(By.CLASS_NAME,'product-stock ')
     splitText1=(stokMiktar.text).split('+')
     splitText2=splitText1[0]
     splitText3=splitText2.split(' ')
-    print(f"Stok Miktarı:{splitText3[-1]}")
+    
     imageArr=[]
     images = driver.find_elements(By.CLASS_NAME, 'owl-item') 
     for i in images:
@@ -54,21 +58,49 @@ def get_data():
         if a != None:    
             # print(f"YENİ URL:{a}") 
             imageArr.append(a)
-    print(f"Resimler:{imageArr}")
+    
     price=driver.find_element(By.CLASS_NAME, 'product-price-item-price').find_element(By.TAG_NAME,'strong')
-    print(f"Özel Fiyat :{price.text}")
+    
+    dataId = driver.find_element(By.CLASS_NAME,'add-to-card-group').find_element(By.CLASS_NAME,'button-addto-card').get_attribute('data-productid_')
+   
     table=driver.find_element(By.CLASS_NAME, 'table-1')
     row=table.find_elements(By.TAG_NAME,'tr')
+
+    tabloList=[]
     for i in row:
         col=i.find_element(By.TAG_NAME,'th')
         col2=i.find_element(By.TAG_NAME,'td')
-        print(f"{col.text} : {col2.text}")
-    print("----------------------------------------------------------------")
-    print("----------------------------------------------------------------")
+    
+        my_dict={
+            col.text : col2.text} 
+        tabloList.append(my_dict)
+    
     time.sleep(1)
-driver.find_element(By.XPATH,"/html/body/main/section[2]/div/div/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/a").click()
+    
+    dicYapisi = {"ürünID":dataId,"ürün ": pruduct.text,"description": smallTitle.text ,"stock": splitText3[-1],"images": [imageArr],"price": price.text,"tabloDeger":tabloList
+     
+}
+    
+    GenelList.append(dicYapisi)  
+driver.find_element(By.XPATH,'/html/body/main/section[2]/div/div/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/a').click()
 get_data()
 
+kok_adi = "URUNLER"
+kok = ET.Element(kok_adi)
+for ogeler in GenelList:
+    ET.SubElement(kok, "Urun").text = str(ogeler)
+    # dict for dönügüsü
+
+
+tree = ET.ElementTree(kok)
+tree.write("app.xml", encoding="utf-8", xml_declaration=True)    
+ 
+# with open("app.xml", "w",encoding='utf-8') as dosya:
+#     dosya.write(str(GenelList))
+   
+
+    
+    
 # while True:
     
 #     content = driver.find_elements(By.CLASS_NAME, "js_obasket_parents")
@@ -87,9 +119,10 @@ get_data()
 #         time.sleep(1)
 #         yeniClick.click()
 #         get_data()
-        
 #         driver.back()
 #         time.sleep(2)
+
+#         print(i-1,". ÜRÜN VERİSi ÇEKİLDİ")
 #         i += 1
         
 #     if i > 61:
@@ -97,3 +130,5 @@ get_data()
 #         print("SIRADAKİ SAYFAYA GEÇİLDİ")
 #         time.sleep(2)
             
+print(GenelList) 
+print(len(GenelList)) 
